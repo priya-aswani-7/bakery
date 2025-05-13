53707b84a955fcd6f86c6b6d867778f7587c8952
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-13 13:50:47 +0400

    Add base.html template to extend from

--- main_app/views.py
+++ main_app/views.py
@@ -4,27 +4,27 @@ from .models import Cake
 
 class CakeListView(ListView):
     model = Cake
-    template_name = 'cake_list.html'
+    template_name = 'cakes/cake_list.html'
     context_object_name = 'cakes'
 
 class CakeDetailView(DetailView):
     model = Cake
-    template_name = 'cake_detail.html'
+    template_name = 'cakes/cake_detail.html'
     context_object_name = 'cake'
 
 class CakeCreateView(CreateView):
     model = Cake
-    template_name = 'cake_form.html'
+    template_name = 'cakes/cake_form.html'
     fields = '__all__'
 
 class CakeUpdateView(UpdateView):
     model = Cake
-    template_name = 'cake_form.html'
+    template_name = 'cakes/cake_form.html'
     fields = '__all__'
     success_url = reverse_lazy('cake_list')
 
 class CakeDeleteView(DeleteView): 
     model = Cake
-    template_name = 'cake_confirm_delete.html'
+    template_name = 'cakes/cake_confirm_delete.html'
     context_object_name = 'cake'
     success_url = reverse_lazy('cake_list')
