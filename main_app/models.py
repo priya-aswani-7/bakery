49af22c9faa45e3faeaf130c2fd555f78256e9d7
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-15 15:32:21 +0400

    Update models

--- main_app/models.py
+++ main_app/models.py
@@ -7,7 +7,7 @@ class Cake(models.Model):
     flavor = models.CharField(max_length=100)
     calories = models.DecimalField(max_digits=6, decimal_places=2)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    quantity_available = models.IntegerField()
+    available = models.BooleanField(default=True)
 
     def __str__(self):
         return f"{self.name} ({self.flavor})"
@@ -23,11 +23,16 @@ class Customer(models.Model):
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     order_date = models.DateTimeField(default = timezone.now)
-    total_price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    @property
+    def total_price(self):
+        order_items = self.orderitem_set.all()
+        total = sum(item.quantity * item.cake.price for item in order_items)
+        return total
 
     def __str__(self):
         return f"Order {self.id} by {self.customer.name}"
-
+    
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
