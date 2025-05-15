ac5af4e2fd3e62288d0d2ecbda1fa90fbf3086ff
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-15 22:23:01 +0400

    Setup views for cake

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import ListView, CreateView, DetailView, EditView, DeleteView
+from django.utils import reverse_lazy
+from models import Cake
 
 # Create your views here.
+
+# list view of cakes
+class CakeListView(ListView):
+    model=Cake
+    template_name='./cakes/cake_list.html'
+    context_object_name='cakes'
+
+# detailed view of a single cake
+class CakeDetailView(DetailView):
+    model=Cake
+    template_name='./cakes/cake_detail.html'
+    context_object_name='cake'
+    
+# create cake
+class CakeCreateView(CreateView):
+    model=Cake
+    template_name='./cakes/cake_form.html'
+    success_url=reverse_lazy('cake_list')
+
+# edit cake 
+class CakeEditView(EditView):
+    model=Cake
+    template_name='./cakes/cake_form.html'
+    context_object_name='cake'
+    success_url=reverse_lazy('cake_detail')
+    
+# delete cake
+class CakeDeleteView(DeleteView):
+    model=Cake
+    template_name='./cakes/cake_confirm_delete.html'
+    context_object_name='cake'
+    success_url=reverse_lazy('cake_list')
