7e6a70a18f7497def60f367674bac7459cefc437
priya-aswani-7 <priya.aswani28139@gmail.com>
2025-05-15 21:43:07 +0400

    Override __str__ method in each model

--- main_app/models.py
+++ main_app/models.py
@@ -6,17 +6,29 @@ class Cake(models.Model):
     flavor = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     available = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.name} ({self.flavor})"
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
+    
+    def __str__(self):
+        return f"{self.name}"
 
 class Order(models.Model):
     order_date = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-
+    
+    def __str__(self):
+        return f"Order #{self.id} by {self.customer.name}"
+        
 class OrderItem(models.Model):
     quantity = models.IntegerField(default=1)
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     cake = models.ForeignKey(Cake, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"Order #{self.order.id} - {self.cake.name} x {self.quantity}"
